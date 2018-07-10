d8eeb7611f09069b558059a1246edd86ed9e7731
Federico García-Lorca <fedegarcia@icloud.com>
2018-07-10 21:22:05 +0200

    Puerto e ip correcto

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app');  
-const port = 3030;
+var ipaddress = process.env.HOST || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    port      = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-app.listen(port, function() {  
+app.listen(port, ipaddress, function() {  
     console.log('Express server listening on port ' + port);
 });
